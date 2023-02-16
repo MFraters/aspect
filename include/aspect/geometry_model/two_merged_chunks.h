48e39e315bebc9d1b2fd073ba05f93e2fccc7f06
Menno Fraters <menno.fraters@outlook.com>
2023-02-16 00:34:20 -0600

    changes to two_merged_chunks for one grid and more flexable boundary indicator locations.

--- include/aspect/geometry_model/two_merged_chunks.h
+++ include/aspect/geometry_model/two_merged_chunks.h
@@ -321,6 +321,8 @@ namespace aspect
          */
         internal::ChunkGeometry<dim> manifold;
 
+        bool two_mergd_grids;
+
         /**
          * Bind boundary indicators to child cells after each mesh refinement round.
          */
--- source/geometry_model/two_merged_chunks.cc
+++ source/geometry_model/two_merged_chunks.cc
@@ -165,14 +165,21 @@ namespace aspect
           if (dim==3)
             {
               // Set the upper part of the southern boundary to indicator 2*dim+2.
-              if (cell->face(4)->at_boundary())
+              if (cell->face(4)->at_boundary()){
                 if ((cell->vertex(cell->face(4)->n_vertices()-1).norm() + cell->vertex(0).norm()) / 2.0 > point3[0])
-                  cell->face(4)->set_boundary_id (2*dim+2);
-
+                  if (manifold.pull_back(cell->face(4)->center(true))[1] < (point2[1]+point1[1])/2.0)
+                    cell->face(4)->set_boundary_id (2 * dim); // set to western
+                  else
+                    cell->face(4)->set_boundary_id (2*dim+2);
+              }
               // Set the upper part of the northern boundary to indicator 2*dim+3.
-              if (cell->face(5)->at_boundary())
+              if (cell->face(5)->at_boundary()){
                 if ((cell->vertex((cell->face(5)->n_vertices()-1)/2).norm()  + cell->vertex(0).norm()) / 2.0 > point3[0])
-                  cell->face(5)->set_boundary_id (2*dim+3);
+                  if (manifold.pull_back(cell->face(5)->center(true))[1] > (point2[1]+point1[1])/2.0)
+                    cell->face(5)->set_boundary_id (2 * dim + 1); // set to eastern
+                  else
+                    cell->face(5)->set_boundary_id (2*dim+3);
+                }
             }
 
         }
